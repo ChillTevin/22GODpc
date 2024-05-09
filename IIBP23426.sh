e17d449944fc09b40fe1ce602390a72e996bf387
ChillTevin <169070677+ChillTevin@users.noreply.github.com>
2024-05-09 16:46:35 -0300

    Update IIBP23426.sh

--- IIBP23426.sh
+++ IIBP23426.sh
@@ -4,7 +4,7 @@
 sudo apt update
 sudo apt install -y libgl1-mesa-dri:i386 libgl1-mesa-glx:i386 libc6:i386
 sudo wget https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb -O /tmp/steam.deb
-sudo dpkg -i /tmp/steam.deb 
+sudo dpkg --install /tmp/steam.deb 
 
 # Configurar el inicio automático en el modo Big Picture de Steam
 echo "Exec=steam -bigpicture" >> ~/.config/autostart/steam.desktop
